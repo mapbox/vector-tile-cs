5dbff88d087358efc9501004ff87ffb961224f63
bergwerkgis <wb@bergwerk-gis.at>
2016-10-26 17:36:49 -0400

    more tests

--- src/VectorTiles.Tests/TestMvtInBulk.cs
+++ src/VectorTiles.Tests/TestMvtInBulk.cs
@@ -5,11 +5,12 @@ using System.Diagnostics;
 using System.Collections.Generic;
 using Mapbox.VectorTile;
 using System.Collections;
+using Mapbox.VectorTile.Geometry;
 
 namespace VectorTiles.Tests
 {
     [TestFixture]
-    public class TestData
+    public class BulkMvtTests
     {
 
         private string fixturesPath;
@@ -21,10 +22,10 @@ namespace VectorTiles.Tests
             fixturesPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "test", "mvt-fixtures", "fixtures", "valid");
         }
 
-        [Test]
+        [Test, Order(1)]
         public void FixturesPathExists()
         {
-            Assert.True(Directory.Exists(fixturesPath));
+            Assert.True(Directory.Exists(fixturesPath), "MVT fixtures directory exists");
         }
 
 
@@ -32,14 +33,42 @@ namespace VectorTiles.Tests
         public void AtLeastOneLayer(string fileName)
         {
             string fullFileName = Path.Combine(fixturesPath, fileName);
-            Assert.True(File.Exists(fullFileName));
+            Assert.True(File.Exists(fullFileName), "Vector tile exists");
             byte[] data = File.ReadAllBytes(fullFileName);
             VectorTile vt = VectorTileReader.Decode(0, 0, 0, data);
-            Assert.GreaterOrEqual(vt.Layers.Count, 1);
+            Assert.GreaterOrEqual(vt.Layers.Count, 1, "At least one layer");
         }
 
 
+        [Test, TestCaseSource(typeof(GetMVTs), "GetFixtureFileName")]
+        public void IterateAllProperties(string fileName)
+        {
+            string fullFileName = Path.Combine(fixturesPath, fileName);
+            Assert.True(File.Exists(fullFileName), "Vector tile exists");
+            byte[] data = File.ReadAllBytes(fullFileName);
+            VectorTile vt = VectorTileReader.Decode(0, 0, 0, data);
+            foreach (var lyr in vt.Layers)
+            {
+                foreach (var feat in lyr.Features)
+                {
+                    var properties = feat.GetProperties();
+                    foreach (var prop in properties)
+                    {
+                        Assert.IsInstanceOf<string>(prop.Key);
+                        Debug.WriteLine(new { prop.Key, prop.Value });
+                    }
+                    foreach (var geom in feat.Geometry)
+                    {
+                        Debug.WriteLine(geom.Count);
+                    }
+                }
+            }
+        }
     }
+
+
+
+
 
 
     public class GetMVTs
---
+++ src/VectorTiles.Tests/TestMvtSingle.cs
@@ -0,0 +1,46 @@
+﻿using Mapbox.VectorTile;
+using Mapbox.VectorTile.Geometry;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace VectorTiles.Tests
+{
+    [TestFixture]
+    public class SingleMvtTests
+    {
+
+        private string fixturesPath;
+
+        [OneTimeSetUp]
+        protected void SetUp()
+        {
+            fixturesPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "test", "mvt-fixtures", "fixtures", "valid");
+        }
+
+
+        [Test]
+        public void FeatureSinglePoint()
+        {
+
+            byte[] data = File.ReadAllBytes(Path.Combine(fixturesPath, "Feature-single-point.mvt"));
+            VectorTile vt = VectorTileReader.Decode(0, 0, 0, data);
+            Assert.AreEqual(1, vt.Layers.Count, "one layer");
+            VectorTileLayer lyr = vt.Layers[0];
+            Assert.AreEqual("layer_name", lyr.Name, "Layer name");
+            Assert.AreEqual(1, lyr.Features.Count, "Feature count");
+            VectorTileFeature feat = lyr.Features[0];
+            Assert.AreEqual(GeomType.POINT, feat.GeometryType, "Geometry type");
+            Assert.AreEqual(123, feat.Id, "id");
+            Dictionary<string, object> properties = feat.GetProperties();
+            Assert.AreEqual("world", properties["hello"]);
+            Assert.AreEqual("world", feat.GetValue("hello"));
+        }
+
+
+
+
+
+    }
+}
