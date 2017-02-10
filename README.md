048ce8239500a4f1dd5a9a96b4a7918f0986c29f
bergwerkgis <wb@bergwerk-gis.at>
2017-02-10 10:19:39 +0100

    update ReadMe, add test with clipping

--- README.md
+++ README.md
@@ -1,7 +1,53 @@
 # vector-tile-cs
-Parses vector tiles with C#.
 
-master:
-* [![Build Status](https://travis-ci.com/mapbox/vector-tile-cs.svg?token=hLpUd9oZwpjSs5JzfqFa&branch=master)](https://travis-ci.com/mapbox/vector-tile-cs)
-* [![Build status](https://ci.appveyor.com/api/projects/status/k8u69w8u13f7i0a7/branch/master?svg=true)](https://ci.appveyor.com/project/Mapbox/vector-tile-cs/branch/master)
-* [![Coverage Status](https://coveralls.io/repos/github/mapbox/vector-tile-cs/badge.svg?branch=master&t=q2Lud9)](https://coveralls.io/github/mapbox/vector-tile-cs?branch=master)
+C# library for decoding [Mapbox Vector Tiles](https://www.mapbox.com/vector-tiles/) ([vector tile specification](https://github.com/mapbox/vector-tile-spec)).
+
+Available as nuget package: [![nuget.org](https://img.shields.io/nuget/v/Mapbox.VectorTile.svg)](https://www.nuget.org/packages/Mapbox.VectorTile)
+
+Vector tile parsers in other languages:
+* JavaScript: https://github.com/mapbox/vector-tile-js
+* C++: https://github.com/mapbox/vector-tile
+
+# Build status
+master branch:
+* Travis: [![Build Status](https://travis-ci.com/mapbox/vector-tile-cs.svg?token=hLpUd9oZwpjSs5JzfqFa&branch=master)](https://travis-ci.com/mapbox/vector-tile-cs)
+* AppVeyor: [![Build status](https://ci.appveyor.com/api/projects/status/k8u69w8u13f7i0a7/branch/master?svg=true)](https://ci.appveyor.com/project/Mapbox/vector-tile-cs/branch/master)
+* Coveralls: [![Coverage Status](https://coveralls.io/repos/github/mapbox/vector-tile-cs/badge.svg?branch=master&t=q2Lud9)](https://coveralls.io/github/mapbox/vector-tile-cs?branch=master)
+
+# Depends
+
+Native C# implementation - no dependencies.
+`NUnit` and `NUnit3TestAdapter` for running tests - will get restored automatically when building the solution.
+
+#Example
+
+```c#
+byte[] data = //raw unzipped vectortile
+VectorTile vt = new VectorTile(data);
+//get available layer names
+foreach(var lyrName in vt.LayerNames()) {
+    //get layer by name
+    VectorTileLayer lyr = vt.GetLayer(lyrName);
+    //iterate through all features
+    for(int i = 0; i < lyr.FeatureCount(); i++) {
+        Debug.WriteLine("{0} lyr:{1} feat:{2}", fileName, lyr.Name, i);
+        //get the feature
+        VectorTileFeature feat = lyr.GetFeature(i);
+        //get feature properties
+        var properties = feat.GetProperties();
+        foreach(var prop in properties) {
+            Debug.WriteLine("key:{0} value:{1}", prop.Key, prop.Value);
+        }
+        //or get property value if you already know the key
+        //object value = feat.GetValue(prop.Key);
+        //iterate through all geometry parts
+        foreach(var part in feat.Geometry) {
+            //iterate through coordinates of the part
+            foreach(var geom in part) {
+                Debug.WriteLine("geom.X:{0} geom.Y:{1}", geom.X, geom.Y);
+            }
+        }
+    }
+}
+
+```
--- src/VectorTiles.Tests/TestMvtInBulk.cs
+++ src/VectorTiles.Tests/TestMvtInBulk.cs
@@ -8,134 +8,139 @@ using System.Collections;
 using Mapbox.VectorTile.Geometry;
 using Mapbox.VectorTile.ExtensionMethods;
 
-namespace VectorTiles.Tests
-{
-    [TestFixture]
-    public class BulkMvtTests
-    {
+namespace VectorTiles.Tests {
+	[TestFixture]
+	public class BulkMvtTests {
 
-        private string fixturesPath;
+		private string fixturesPath;
 
 
-        [OneTimeSetUp]
-        protected void SetUp()
-        {
-            fixturesPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "test", "mvt-fixtures", "fixtures", "valid");
-        }
+		[OneTimeSetUp]
+		protected void SetUp() {
+			fixturesPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "test", "mvt-fixtures", "fixtures", "valid");
+		}
 
-        [Test, Order(1)]
-        public void FixturesPathExists()
-        {
-            Assert.True(Directory.Exists(fixturesPath), "MVT fixtures directory exists");
-        }
+		[Test, Order(1)]
+		public void FixturesPathExists() {
+			Assert.True(Directory.Exists(fixturesPath), "MVT fixtures directory exists");
+		}
 
 
-        [Test, TestCaseSource(typeof(GetMVTs), "GetFixtureFileName")]
-        public void LazyDecoding(string fileName)
-        {
-            string fullFileName = Path.Combine(fixturesPath, fileName);
-            Assert.True(File.Exists(fullFileName), "Vector tile exists");
-            byte[] data = File.ReadAllBytes(fullFileName);
-            VectorTile vt = new VectorTile(data);
-            foreach (var layerName in vt.LayerNames())
-            {
-                VectorTileLayer layer = vt.GetLayer(layerName);
-                for (int i = 0; i < layer.FeatureCount(); i++)
-                {
-                    VectorTileFeature feat = layer.GetFeature(i);
-                    var properties = feat.GetProperties();
-                    foreach (var prop in properties)
-                    {
-                        Assert.AreEqual(prop.Value, feat.GetValue(prop.Key), "Property values match");
-                    }
-                    foreach (var geomPart in feat.Geometry)
-                    {
-                        foreach (var coord in geomPart)
-                        {
-                            //TODO add Assert
-                        }
-                    }
-                }
-            }
-            string geojson = vt.ToGeoJson(0, 0, 0);
-            Assert.GreaterOrEqual(geojson.Length, 30, "geojson >= 30 chars");
-        }
+		[Test, TestCaseSource(typeof(GetMVTs), "GetFixtureFileName")]
+		public void LazyDecoding(string fileName) {
+			string fullFileName = Path.Combine(fixturesPath, fileName);
+			Assert.True(File.Exists(fullFileName), "Vector tile exists");
+			byte[] data = File.ReadAllBytes(fullFileName);
+			VectorTile vt = new VectorTile(data);
+			foreach(var layerName in vt.LayerNames()) {
+				VectorTileLayer layer = vt.GetLayer(layerName);
+				for(int i = 0; i < layer.FeatureCount(); i++) {
+					VectorTileFeature feat = layer.GetFeature(i);
+					var properties = feat.GetProperties();
+					foreach(var prop in properties) {
+						Assert.AreEqual(prop.Value, feat.GetValue(prop.Key), "Property values match");
+					}
+					foreach(var geomPart in feat.Geometry) {
+						foreach(var coord in geomPart) {
+							//TODO add Assert
+						}
+					}
+				}
+			}
+			string geojson = vt.ToGeoJson(0, 0, 0);
+			Assert.GreaterOrEqual(geojson.Length, 30, "geojson >= 30 chars");
+		}
 
 
-        [Test, TestCaseSource(typeof(GetMVTs), "GetFixtureFileName")]
-        public void AtLeastOneLayer(string fileName)
-        {
-            string fullFileName = Path.Combine(fixturesPath, fileName);
-            Assert.True(File.Exists(fullFileName), "Vector tile exists");
-            byte[] data = File.ReadAllBytes(fullFileName);
-            VectorTile vt = new VectorTile(data);
-            Assert.GreaterOrEqual(vt.LayerNames().Count, 1, "At least one layer");
-            string geojson = vt.ToGeoJson(0, 0, 0);
-            Assert.GreaterOrEqual(geojson.Length, 30, "geojson >= 30 chars");
-            foreach (var lyrName in vt.LayerNames())
-            {
-                VectorTileLayer lyr = vt.GetLayer(lyrName);
-                for (int i = 0; i < lyr.FeatureCount(); i++)
-                {
-                    Debug.WriteLine("{0} lyr:{1} feat:{2}", fileName, lyr.Name, i);
-                    VectorTileFeature feat = lyr.GetFeature(i);
-                    long extent = (long)lyr.Extent;
-                    foreach (var part in feat.Geometry)
-                    {
-                        foreach (var geom in part)
-                        {
-                            if (geom.X < 0 || geom.Y < 0 || geom.X > extent || geom.Y > extent)
-                            {
-                                Debug.WriteLine("{0} lyr:{1} feat:{2} x:{3} y:{4}", fileName, lyr.Name, i, geom.X, geom.Y);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+		[Test, TestCaseSource(typeof(GetMVTs), "GetFixtureFileName")]
+		public void AtLeastOneLayer(string fileName) {
+			string fullFileName = Path.Combine(fixturesPath, fileName);
+			Assert.True(File.Exists(fullFileName), "Vector tile exists");
+			byte[] data = File.ReadAllBytes(fullFileName);
+			VectorTile vt = new VectorTile(data);
+			Assert.GreaterOrEqual(vt.LayerNames().Count, 1, "At least one layer");
+			string geojson = vt.ToGeoJson(0, 0, 0);
+			Assert.GreaterOrEqual(geojson.Length, 30, "geojson >= 30 chars");
+			foreach(var lyrName in vt.LayerNames()) {
+				VectorTileLayer lyr = vt.GetLayer(lyrName);
+				for(int i = 0; i < lyr.FeatureCount(); i++) {
+					Debug.WriteLine("{0} lyr:{1} feat:{2}", fileName, lyr.Name, i);
+					VectorTileFeature feat = lyr.GetFeature(i);
+					long extent = (long)lyr.Extent;
+					foreach(var part in feat.Geometry) {
+						foreach(var geom in part) {
+							if(geom.X < 0 || geom.Y < 0 || geom.X > extent || geom.Y > extent) {
+								Debug.WriteLine("{0} lyr:{1} feat:{2} x:{3} y:{4}", fileName, lyr.Name, i, geom.X, geom.Y);
+							}
+						}
+					}
+				}
+			}
+		}
 
 
-        [Test, TestCaseSource(typeof(GetMVTs), "GetFixtureFileName")]
-        public void IterateAllProperties(string fileName)
-        {
-            string fullFileName = Path.Combine(fixturesPath, fileName);
-            Assert.True(File.Exists(fullFileName), "Vector tile exists");
-            byte[] data = File.ReadAllBytes(fullFileName);
-            VectorTile vt = new VectorTile(data);
-            foreach (var layerName in vt.LayerNames())
-            {
-                var layer = vt.GetLayer(layerName);
-                for (int i = 0; i < layer.FeatureCount(); i++)
-                {
-                    var feat = layer.GetFeature(i);
-                    var properties = feat.GetProperties();
-                    foreach (var prop in properties)
-                    {
-                        Assert.IsInstanceOf<string>(prop.Key);
-                    }
-                }
-            }
-        }
-    }
+		[Test, TestCaseSource(typeof(GetMVTs), "GetFixtureFileName")]
+		public void ClipHardAtTileBoundary(string fileName) {
+			string fullFileName = Path.Combine(fixturesPath, fileName);
+			Assert.True(File.Exists(fullFileName), "Vector tile exists");
+			byte[] data = File.ReadAllBytes(fullFileName);
+			VectorTile vt = new VectorTile(data);
+			Assert.GreaterOrEqual(vt.LayerNames().Count, 1, "At least one layer");
+			string geojson = vt.ToGeoJson(0, 0, 0);
+			Assert.GreaterOrEqual(geojson.Length, 30, "geojson >= 30 chars");
+			foreach(var lyrName in vt.LayerNames()) {
+				VectorTileLayer lyr = vt.GetLayer(lyrName);
+				for(int i = 0; i < lyr.FeatureCount(); i++) {
+					Debug.WriteLine("{0} lyr:{1} feat:{2}", fileName, lyr.Name, i);
+					VectorTileFeature feat = lyr.GetFeature(i, 0);
+					long extent = (long)lyr.Extent;
+					foreach(var part in feat.Geometry) {
+						foreach(var geom in part) {
+							Assert.GreaterOrEqual(geom.X, 0, "geom.X equal or greater 0");
+							Assert.GreaterOrEqual(geom.Y, 0, "geom.Y eqaul or greater 0");
+							Assert.LessOrEqual(geom.X, extent, "geom.X less or equal extent");
+							Assert.LessOrEqual(geom.Y, extent, "geom.Y less or equal extent");
+						}
+					}
+				}
+			}
+		}
+
+
+		[Test, TestCaseSource(typeof(GetMVTs), "GetFixtureFileName")]
+		public void IterateAllProperties(string fileName) {
+			string fullFileName = Path.Combine(fixturesPath, fileName);
+			Assert.True(File.Exists(fullFileName), "Vector tile exists");
+			byte[] data = File.ReadAllBytes(fullFileName);
+			VectorTile vt = new VectorTile(data);
+			foreach(var layerName in vt.LayerNames()) {
+				var layer = vt.GetLayer(layerName);
+				for(int i = 0; i < layer.FeatureCount(); i++) {
+					var feat = layer.GetFeature(i);
+					var properties = feat.GetProperties();
+					foreach(var prop in properties) {
+						Assert.IsInstanceOf<string>(prop.Key);
+					}
+				}
+			}
+		}
+	}
 
 
 
 
 
 
-    public partial class GetMVTs
-    {
-        public static IEnumerable GetFixtureFileName()
-        {
-            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "test", "mvt-fixtures", "fixtures", "valid");
+	public partial class GetMVTs {
+		public static IEnumerable GetFixtureFileName() {
+			string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "test", "mvt-fixtures", "fixtures", "valid");
 
-            foreach (var file in Directory.GetFiles(path))
-            {
-                //return file basename only to make test description more readable
-                yield return new TestCaseData(Path.GetFileName(file));
-            }
-        }
-    }
+			foreach(var file in Directory.GetFiles(path)) {
+				//return file basename only to make test description more readable
+				yield return new TestCaseData(Path.GetFileName(file));
+			}
+		}
+	}
 
 
 
