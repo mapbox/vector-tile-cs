8ee9e59f8f5417d3e38c688d360e0a77c71e635d
bergwerkgis <wb@bergwerk-gis.at>
2017-03-24 15:11:02 +0100

    let DemoConsoleApp display data instead of outputting GeoJSON

--- src/DemoConsoleApp/program.cs
+++ src/DemoConsoleApp/program.cs
@@ -1,5 +1,6 @@
 using Mapbox.VectorTile.ExtensionMethods;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 
@@ -11,17 +12,17 @@ namespace Mapbox.VectorTile {
 
 		public static int Main(string[] args) {
 
-			if(args.Length != 1 && args.Length != 2) {
+			if (args.Length != 1 && args.Length != 2) {
 				Console.WriteLine("invalid number of arguments");
 				return 1;
 			}
 
 			string vtIn = args[0];
 			uint? clipBuffer = null;
-			if(args.Length == 2) {
+			if (args.Length == 2) {
 				clipBuffer = Convert.ToUInt32(args[1]);
 			}
-			if(!File.Exists(vtIn)) {
+			if (!File.Exists(vtIn)) {
 				Console.WriteLine($"file [{vtIn}] not found");
 				return 1;
 			}
@@ -32,13 +33,28 @@ namespace Mapbox.VectorTile {
 			ulong tileCol;
 			ulong tileRow;
 
-			if(!parseArg(Path.GetFileName(vtIn), out zoom, out tileCol, out tileRow)) {
+			if (!parseArg(Path.GetFileName(vtIn), out zoom, out tileCol, out tileRow)) {
 				return 1;
 			}
 
 			VectorTile tile = new VectorTile(bufferedData);
 
-			Console.WriteLine(tile.ToGeoJson(zoom, tileCol, tileRow, clipBuffer));
+			//Console.WriteLine(tile.ToGeoJson(zoom, tileCol, tileRow, clipBuffer));
+
+			foreach (string lyrName in tile.LayerNames()) {
+				VectorTileLayer lyr = tile.GetLayer(lyrName);
+				Console.WriteLine(string.Format("------------ {0} ---------", lyrName));
+				//if (lyrName != "building") { continue; }
+				int featCnt = lyr.FeatureCount();
+				for (int i = 0; i < featCnt; i++) {
+					VectorTileFeature feat = lyr.GetFeature(i);
+					Console.WriteLine(string.Format("feature {0}: {1}", i, feat.GeometryType));
+					Dictionary<string, object> props = feat.GetProperties();
+					foreach (var prop in props) {
+						Console.WriteLine(string.Format("   {0}\t : {1}", prop.Key, prop.Value));
+					}
+				}
+			}
 
 			return 0;
 		}
@@ -51,19 +67,19 @@ namespace Mapbox.VectorTile {
 
 			string zxyTxt = fileName.Split(".".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[0];
 			string[] zxy = zxyTxt.Split("-".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-			if(zxy.Length != 3) {
+			if (zxy.Length != 3) {
 				Console.WriteLine("invalid zoom, tileCol or tileRow [{0}]", zxyTxt);
 				return false;
 			}
-			if(!ulong.TryParse(zxy[0], out zoom)) {
+			if (!ulong.TryParse(zxy[0], out zoom)) {
 				Console.WriteLine($"could not parse zoom: {zxy[0]}");
 				return false;
 			}
-			if(!ulong.TryParse(zxy[1], out tileCol)) {
+			if (!ulong.TryParse(zxy[1], out tileCol)) {
 				Console.WriteLine($"could not parse tileCol: {zxy[1]}");
 				return false;
 			}
-			if(!ulong.TryParse(zxy[2], out tileRow)) {
+			if (!ulong.TryParse(zxy[2], out tileRow)) {
 				Console.WriteLine($"could not parse tileRow: {zxy[2]}");
 				return false;
 			}
