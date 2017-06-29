7ffd8fd27adfc18a4476dcefcca35f844f3e1051
BergWerkGIS <wb@BergWerk-GIS.at>
2017-06-29 14:08:07 +0200

    add GetPackedSInt32() and GetPackedSInt64()

--- src/PbfReader/PbfReader.cs
+++ src/PbfReader/PbfReader.cs
@@ -4,7 +4,8 @@ using System.Text;
 using Mapbox.VectorTile.Contants;
 
 
-namespace Mapbox.VectorTile {
+namespace Mapbox.VectorTile
+{
 
 
 	//TODO: implement DataView using the same byte array instead of copying byte arrays
@@ -17,7 +18,8 @@ namespace Mapbox.VectorTile {
 	/// <summary>
 	/// Low level protobuf (PBF) decoder https://developers.google.com/protocol-buffers/docs/overview
 	/// </summary>
-	public class PbfReader {
+	public class PbfReader
+	{
 
 
 		/// <summary>Tag at current position</summary>
@@ -38,7 +40,8 @@ namespace Mapbox.VectorTile {
 		/// PbfReader constructor
 		/// </summary>
 		/// <param name="tileBuffer">Byte array containing the raw (already unzipped) tile data</param>
-		public PbfReader(byte[] tileBuffer) {
+		public PbfReader(byte[] tileBuffer)
+		{
 			_buffer = tileBuffer;
 			_length = (ulong)_buffer.Length;
 			WireType = WireTypes.UNDEFINED;
@@ -50,16 +53,19 @@ namespace Mapbox.VectorTile {
 		/// <para>Throws exception if Varint cannot be decoded</para>
 		/// </summary>
 		/// <returns>Decoded Varint</returns>
-		public long Varint() {
+		public long Varint()
+		{
 			// convert to base 128 varint
 			// https://developers.google.com/protocol-buffers/docs/encoding
 			int shift = 0;
 			long result = 0;
-			while (shift < 64) {
+			while (shift < 64)
+			{
 				byte b = _buffer[_pos];
 				result |= (long)(b & 0x7F) << shift;
 				_pos++;
-				if ((b & 0x80) == 0) {
+				if ((b & 0x80) == 0)
+				{
 					return result;
 				}
 				shift += 7;
@@ -74,12 +80,15 @@ namespace Mapbox.VectorTile {
 		/// <para>TODO: refactor to return a DataView instead of a byte array</para>
 		/// </summary>
 		/// <returns>Byte array containing the view</returns>
-		public byte[] View() {
+		public byte[] View()
+		{
 			// return layer/feature subsections of the main stream
-			if (Tag == 0) {
+			if (Tag == 0)
+			{
 				throw new System.Exception("call next() before accessing field value");
 			};
-			if (WireType != WireTypes.BYTES) {
+			if (WireType != WireTypes.BYTES)
+			{
 				throw new System.Exception("not of type string, bytes or message");
 			}
 
@@ -98,15 +107,54 @@ namespace Mapbox.VectorTile {
 		/// Get repeated `uint`s a current position, move position
 		/// </summary>
 		/// <returns>List of decoded `uint`s</returns>
-		public List<uint> GetPackedUnit32() {
+		public List<uint> GetPackedUnit32()
+		{
 			List<uint> values = new List<uint>(200);
 			ulong sizeInByte = (ulong)Varint();
 			ulong end = _pos + sizeInByte;
-			while (_pos < end) {
-				ulong val = (ulong)Varint();
-				values.Add((uint)val);
+			while (_pos < end)
+			{
+				values.Add((uint)Varint());
 			}
 			return values;
+		}
+
+
+		public List<int> GetPackedSInt32()
+		{
+			List<int> values = new List<int>(200);
+			ulong sizeInByte = (ulong)Varint();
+			ulong end = _pos + sizeInByte;
+			while (_pos < end)
+			{
+				values.Add(decodeZigZag32((int)Varint()));
+			}
+			return values;
+		}
+
+
+		public List<long> GetPackedSInt64()
+		{
+			List<long> values = new List<long>(200);
+			ulong sizeInByte = (ulong)Varint();
+			ulong end = _pos + sizeInByte;
+			while (_pos < end)
+			{
+				values.Add(decodeZigZag64((long)Varint()));
+			}
+			return values;
+		}
+
+
+		private int decodeZigZag32(int value)
+		{
+			return (value >> 1) ^ -(value & 1);
+		}
+
+
+		private long decodeZigZag64(long value)
+		{
+			return (value >> 1) ^ -(value & 1);
 		}
 
 
@@ -114,7 +162,8 @@ namespace Mapbox.VectorTile {
 		/// Get double at current position, move to next position
 		/// </summary>
 		/// <returns>Decoded double</returns>
-		public double GetDouble() {
+		public double GetDouble()
+		{
 			byte[] buf = new byte[8];
 			System.Array.Copy(_buffer, (int)_pos, buf, 0, 8);
 			_pos += 8;
@@ -127,7 +176,8 @@ namespace Mapbox.VectorTile {
 		/// Get float a current position, move to next position
 		/// </summary>
 		/// <returns>Decoded float</returns>
-		public float GetFloat() {
+		public float GetFloat()
+		{
 			byte[] buf = new byte[4];
 			System.Array.Copy(_buffer, (int)_pos, buf, 0, 4);
 			_pos += 4;
@@ -141,7 +191,8 @@ namespace Mapbox.VectorTile {
 		/// </summary>
 		/// <param name="length">Number of bytes to read</param>
 		/// <returns>Decoded string</returns>
-		public string GetString(ulong length) {
+		public string GetString(ulong length)
+		{
 			byte[] buf = new byte[length];
 			System.Array.Copy(_buffer, (int)_pos, buf, 0, (int)length);
 			_pos += length;
@@ -153,8 +204,10 @@ namespace Mapbox.VectorTile {
 		/// Move to next byte and set wire type. Throws exeception if tag is out of range
 		/// </summary>
 		/// <returns>Returns false if at end of buffer</returns>
-		public bool NextByte() {
-			if (_pos >= _length) {
+		public bool NextByte()
+		{
+			if (_pos >= _length)
+			{
 				return false;
 			}
 			// get and process the next byte in the buffer
@@ -164,7 +217,8 @@ namespace Mapbox.VectorTile {
 			if (
 				(Tag == 0 || Tag >= 19000)
 				&& (Tag > 19999 || Tag <= ((1 << 29) - 1))
-			) {
+			)
+			{
 				throw new System.Exception("tag out of range");
 			}
 			WireType = (WireTypes)(Value & 0x07);
@@ -175,7 +229,8 @@ namespace Mapbox.VectorTile {
 		/// <summary>
 		/// Skip over a Varint
 		/// </summary>
-		public void SkipVarint() {
+		public void SkipVarint()
+		{
 			Varint();
 			//while (0 == (_buffer[Pos] & 0x80))
 			//{
@@ -197,8 +252,10 @@ namespace Mapbox.VectorTile {
 		/// Skip bytes
 		/// </summary>
 		/// <param name="skip">Number of bytes to skip</param>
-		public void SkipBytes(ulong skip) {
-			if (_pos + skip > _length) {
+		public void SkipBytes(ulong skip)
+		{
+			if (_pos + skip > _length)
+			{
 				string msg = string.Format(NumberFormatInfo.InvariantInfo, "[SkipBytes()] skip:{0} pos:{1} len:{2}", skip, _pos, _length);
 				throw new System.Exception(msg);
 			}
@@ -210,12 +267,15 @@ namespace Mapbox.VectorTile {
 		/// Automatically skip bytes based on wire type
 		/// </summary>
 		/// <returns>New position within the byte array</returns>
-		public ulong Skip() {
-			if (Tag == 0) {
+		public ulong Skip()
+		{
+			if (Tag == 0)
+			{
 				throw new System.Exception("call next() before calling skip()");
 			}
 
-			switch (WireType) {
+			switch (WireType)
+			{
 				case WireTypes.VARINT:
 					SkipVarint();
 					break;
